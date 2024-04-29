7723b5ebbd91a5d34cebb1b56b05f933f28af5c1
MoFares7 <faresdabbas1@gmail.com>
2024-04-29 22:34:03 +0300

    upload latest version

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,7 @@ const questionController = require('../controllers/QuestionController');
 router.post('/answers/store', answerController.createAnswer);
 router.get('/answers/index', answerController.getAllAnswer);
 router.get('/answers', answerController.getAnswersByQuestionId);
+router.get('/answer/update', answerController.updateAnswer);
 
 router.post('/question/store', questionController.createNewQuestion);
 router.get('/question/index', questionController.getAllQuestion);
