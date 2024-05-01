c8266888342ed60eaaaa9137f3d16ed3dccee45b
MoFares7 <faresdabbas1@gmail.com>
2024-05-02 00:31:25 +0300

    upload chatBot operation

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('chatbot_z7py', 'chatbot_z7py_user', 'gfkvVY5NGVgQSlkf7uc9iKM9LZSNz9BS', {
-//         host: 'dpg-cooc98f79t8c73bas4mg-a',
-//         dialect: 'postgres',
-//         // password: 'fF1122334455'
-// });
-
-// module.exports = sequelize;
-
-
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('chatbot', 'root', '', {
-        host: 'localhost',
-        dialect: 'mysql',
-        password: 'fF1122334455'
+const sequelize = new Sequelize('chatbot_z7py', 'chatbot_z7py_user', 'gfkvVY5NGVgQSlkf7uc9iKM9LZSNz9BS', {
+        host: 'dpg-cooc98f79t8c73bas4mg-a',
+        dialect: 'postgres',
+        // password: 'fF1122334455'
 });
 
 module.exports = sequelize;
+
+
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('chatbot', 'root', '', {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         password: 'fF1122334455'
+// });
+
+// module.exports = sequelize;
 
